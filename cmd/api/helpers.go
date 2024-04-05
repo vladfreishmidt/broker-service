f3212039cbceb529d398187629869964bb718fe7
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2024-04-05 15:28:06 +0300

    Update broker-service to connect to authentication-service

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -13,7 +13,7 @@ type jsonResponse struct {
 	Data    any    `json:"data,omitempty"`
 }
 
-func (app *Config) readJSON(w http.ResponseWriter, r *http.Response, data any) error {
+func (app *Config) readJSON(w http.ResponseWriter, r *http.Request, data any) error {
 	maxBytes := 1048576 // 1MB
 
 	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
